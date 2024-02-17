e7bdb58d79085a9a303a2730431a0d6c5da78db5
Md <amzad276026@gmail.com>
2024-02-18 01:49:23 +0600

    dist project commit

---
+++ myscripts.js
@@ -0,0 +1,16 @@
+// pop up window
+
+function login() {
+  document.getElementById("myDisplay").style.display = "block";
+  document.getElementById("myDisplay").style.top = "50%";
+}
+function closebtn() {
+  document.getElementById("myDisplay").style.display = "none";
+}
+
+function navOpen() {
+  document.getElementById("nav-container").style.display = "flex";
+}
+function navclose() {
+  document.getElementById("nav-container").style.display = "none";
+}
